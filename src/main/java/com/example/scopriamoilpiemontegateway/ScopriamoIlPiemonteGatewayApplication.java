e7ebb2d4922da5f5674ad0b2b7b69210a5c7cf48
danielegerbaldo <daniele.gerbaldo@gmail.com>
2021-04-21 15:34:24 +0200

    Initial commit

---
+++ src/main/java/com/example/scopriamoilpiemontegateway/ScopriamoIlPiemonteGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.scopriamoilpiemontegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScopriamoIlPiemonteGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScopriamoIlPiemonteGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/scopriamoilpiemontegateway/ScopriamoIlPiemonteGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scopriamoilpiemontegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScopriamoIlPiemonteGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
