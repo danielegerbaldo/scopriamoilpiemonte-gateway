af8a9e6b3a386a17721c5ed10767ad44983306cf
danielegerbaldo <daniele.gerbaldo@gmail.com>
2021-04-30 19:10:52 +0200

    Bugfix in servizio-eventi routing

--- src/main/java/TAASS/scopriamoilpiemontegateway/ScopriamoIlPiemonteGatewayApplication.java
+++ src/main/java/TAASS/scopriamoilpiemontegateway/ScopriamoIlPiemonteGatewayApplication.java
@@ -18,7 +18,7 @@ public class ScopriamoIlPiemonteGatewayApplication {
 		return builder.routes()
 				.route("path_route", r -> r.path("/api/v1/evento/**").and().method("POST", "PUT", "DELETE","GET")
 						.filters(f -> f.addResponseHeader("Cache-Control", "max-age=300")) //Keep items in cache for 5 minutes
-						.uri("http://localhost:8080"))
+						.uri("http://servizio-eventi:8080"))
 				//.route("path_route", r -> r.path("/v1/utente").and().method("POST", "PUT", "DELETE","GET")
 				//		.uri("http://servizio-utenti:8081"))
 				//.route("path_route", r -> r.path("/v1/sondaggio").and().method("POST", "PUT", "DELETE","GET")
