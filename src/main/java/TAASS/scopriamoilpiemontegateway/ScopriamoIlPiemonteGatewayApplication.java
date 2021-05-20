74318da702941afa6551d35639ed6c9ac3466c5b
danielegerbaldo <daniele.gerbaldo@gmail.com>
2021-05-20 20:11:17 +0200

    Eureka discovery service support added

--- src/main/java/TAASS/scopriamoilpiemontegateway/ScopriamoIlPiemonteGatewayApplication.java
+++ src/main/java/TAASS/scopriamoilpiemontegateway/ScopriamoIlPiemonteGatewayApplication.java
@@ -2,17 +2,19 @@ package TAASS.scopriamoilpiemontegateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ScopriamoIlPiemonteGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScopriamoIlPiemonteGatewayApplication.class, args);
 	}
-
+/*
 	@Bean
 	public RouteLocator customRouteLocator(RouteLocatorBuilder builder)  {
 		return builder.routes()
@@ -25,5 +27,5 @@ public class ScopriamoIlPiemonteGatewayApplication {
 				//		.uri("http://servizio-sondaggi:8082"))
 				.build();
 	}
-
+*/
 }
