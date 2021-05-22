18a18aed509f3a744d00226e9809964f140e74a4
danielegerbaldo <daniele.gerbaldo@gmail.com>
2021-05-22 02:45:25 +0200

    Event wrong route fixed

--- src/main/java/TAASS/scopriamoilpiemontegateway/ScopriamoIlPiemonteGatewayApplication.java
+++ src/main/java/TAASS/scopriamoilpiemontegateway/ScopriamoIlPiemonteGatewayApplication.java
@@ -14,18 +14,18 @@ public class ScopriamoIlPiemonteGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ScopriamoIlPiemonteGatewayApplication.class, args);
 	}
-/*
+
 	@Bean
 	public RouteLocator customRouteLocator(RouteLocatorBuilder builder)  {
 		return builder.routes()
 				.route("path_route", r -> r.path("/api/v1/evento/**").and().method("POST", "PUT", "DELETE","GET")
 						.filters(f -> f.addResponseHeader("Cache-Control", "max-age=300")) //Keep items in cache for 5 minutes
-						.uri("http://servizio-eventi:8080"))
+						.uri("lb://EVENT-SERVICE"))
 				//.route("path_route", r -> r.path("/v1/utente").and().method("POST", "PUT", "DELETE","GET")
 				//		.uri("http://servizio-utenti:8081"))
 				//.route("path_route", r -> r.path("/v1/sondaggio").and().method("POST", "PUT", "DELETE","GET")
 				//		.uri("http://servizio-sondaggi:8082"))
 				.build();
 	}
-*/
+
 }
