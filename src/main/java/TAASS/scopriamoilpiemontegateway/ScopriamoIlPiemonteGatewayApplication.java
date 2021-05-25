a2f87887668a44a556aaaa02c96a131d234d3867
danielegerbaldo <daniele.gerbaldo@gmail.com>
2021-05-25 13:28:18 +0200

    Security routes added

--- src/main/java/TAASS/scopriamoilpiemontegateway/ScopriamoIlPiemonteGatewayApplication.java
+++ src/main/java/TAASS/scopriamoilpiemontegateway/ScopriamoIlPiemonteGatewayApplication.java
@@ -20,14 +20,16 @@ public class ScopriamoIlPiemonteGatewayApplication {
 	@Bean
 	public RouteLocator customRouteLocator(RouteLocatorBuilder builder)  {
 		return builder.routes()
-				.route("event-service", r -> r.path("/api/v1/evento/**").and().method("POST", "PUT", "DELETE","GET")
-						.filters(f -> f.addResponseHeader("Cache-Control", "max-age=300")) //Keep items in cache for 5 minutes
-						.uri("lb://EVENT-SERVICE"))
-				.route("user-service", r -> r.path("/api/v1/utente/**").and().method("POST", "PUT", "DELETE","GET")
-						.filters(f -> f.filter(AuthFilter.class,0))
-						.uri("lb://USER-SERVICE"))
-				.route("municipality-service", r -> r.path("/api/v1/comune/**").and().method("POST", "PUT", "DELETE","GET")
-						.uri("lb://MUNICIPALITY-SERVICE"))
+				//.route("registry-service", r -> r.path("/api/v1/signUp").and().method("POST", "PUT", "DELETE","GET")
+				//		.uri("lb://USER-SERVICE"))
+				//.route("event-service", r -> r.path("/api/v1/evento/**").and().method("POST", "PUT", "DELETE","GET")
+				//		.filters(f -> f.addResponseHeader("Cache-Control", "max-age=300")) //Keep items in cache for 5 minutes
+				//		.uri("lb://EVENT-SERVICE"))
+				//.route("user-service", r -> r.path("/api/v1/utente/**").and().method("POST", "PUT", "DELETE","GET")
+				//		.filters(f -> f.filter(AuthFilter.class,0))
+				//		.uri("lb://USER-SERVICE"))
+				//.route("municipality-service", r -> r.path("/api/v1/comune/**").and().method("POST", "PUT", "DELETE","GET")
+				//		.uri("lb://MUNICIPALITY-SERVICE"))
 				//.route("registry-service", r -> r.path("").and().method("POST", "PUT", "DELETE","GET")
 				//		.uri("http://servizio-registry:8010"))
 				.build();
