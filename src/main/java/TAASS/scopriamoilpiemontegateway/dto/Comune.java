0faa6f48ab9294dd25292c90caf12b2455a5aad3
danielegerbaldo <daniele.gerbaldo@gmail.com>
2021-06-18 17:16:00 +0200

    class Comune updated

--- src/main/java/TAASS/scopriamoilpiemontegateway/dto/Comune.java
+++ src/main/java/TAASS/scopriamoilpiemontegateway/dto/Comune.java
@@ -1,22 +1,30 @@
 package TAASS.scopriamoilpiemontegateway.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 public class Comune {
     public Comune() {
     }
 
-    public Comune(Long istat, String nome, String CAP, String provincia) {
+    public Comune(Long istat, String nome, String CAP, String regione, String provincia) {
         this.istat = istat;
         this.nome = nome;
         this.CAP = CAP;
+        this.regione = regione;
         this.provincia = provincia;
     }
 
     private Long istat;
+
     private String nome;
+
     private String CAP;
+
+    private String regione;
+
     private String provincia;
+
+    private Double lat;
+
+    private Double lng;
 
     public Long getIstat() {
         return istat;
@@ -48,5 +56,29 @@ public class Comune {
 
     public void setProvincia(String provincia) {
         this.provincia = provincia;
+    }
+
+    public String getRegione() {
+        return regione;
+    }
+
+    public void setRegione(String regione) {
+        this.regione = regione;
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    public Double getLng() {
+        return lng;
+    }
+
+    public void setLng(Double lng) {
+        this.lng = lng;
     }
 }
