2ab21992ac499c183e575362d8f69c7818d04dff
danielegerbaldo <daniele.gerbaldo@gmail.com>
2021-06-25 19:05:10 +0200

    CORS support added in evento-composed/info-evento and evento-composed/getAllEvent requests

--- src/main/java/TAASS/scopriamoilpiemontegateway/config/events/EventConfiguration.java
+++ src/main/java/TAASS/scopriamoilpiemontegateway/config/events/EventConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
