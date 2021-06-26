7977082393218062b72ae0a56d9db71b90fea3b7
danielegerbaldo <daniele.gerbaldo@gmail.com>
2021-06-26 17:04:49 +0200

    CORS origin fixed

--- src/main/java/TAASS/scopriamoilpiemontegateway/config/CorsConfig.java
+++ src/main/java/TAASS/scopriamoilpiemontegateway/config/CorsConfig.java
@@ -10,9 +10,9 @@ import org.springframework.web.reactive.config.WebFluxConfigurer;
 public class CorsConfig implements WebFluxConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
-        corsRegistry.addMapping("http://localhost:4200")
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .maxAge(3600);
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedHeaders("*")
+                .allowedMethods("*");
     }
 }
